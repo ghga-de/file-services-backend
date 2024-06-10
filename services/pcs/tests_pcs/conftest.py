cc9328dbcfea67e0c4ca9fae997fe8ceb3cdf74e
TheByronHimes <TheByronHimes@gmail.com>
2024-06-10 11:00:04 +0000

    Update tests

---
+++ services/pcs/tests_pcs/conftest.py
@@ -0,0 +1,28 @@
+# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Establish session-scoped fixtures, such as the testcontainer fixtures."""
+
+from hexkit.providers.akafka.testutils import (  # noqa: F401
+    kafka_container_fixture,
+    kafka_fixture,
+)
+from hexkit.providers.mongodb.testutils import (  # noqa: F401
+    mongodb_container_fixture,
+    mongodb_fixture,
+)
+from hexkit.providers.mongokafka.testutils import mongo_kafka_fixture  # noqa: F401
+
+from tests_pcs.fixtures.joint import joint_fixture  # noqa: F401
