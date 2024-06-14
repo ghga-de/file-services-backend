f46c010b5707d3b6cbaed1a28f2676656bb70049
TheByronHimes <TheByronHimes@gmail.com>
2024-06-14 11:42:35 +0000

    Add 'default=' to configs and remove typing.Optional

--- services/ucs/src/ucs/inject.py
+++ services/ucs/src/ucs/inject.py
@@ -17,7 +17,6 @@
 
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
-from typing import Optional
 
 from fastapi import FastAPI
 from ghga_service_commons.utils.context import asyncnullcontext
@@ -64,7 +63,7 @@ async def prepare_core(
 def prepare_core_with_override(
     *,
     config: Config,
-    core_override: Optional[tuple[UploadServicePort, FileMetadataServicePort]] = None,
+    core_override: tuple[UploadServicePort, FileMetadataServicePort] | None = None,
 ):
     """Resolve the prepare_core context manager based on config and override (if any)."""
     return (
@@ -78,7 +77,7 @@ def prepare_core_with_override(
 async def prepare_rest_app(
     *,
     config: Config,
-    core_override: Optional[tuple[UploadServicePort, FileMetadataServicePort]] = None,
+    core_override: tuple[UploadServicePort, FileMetadataServicePort] | None = None,
 ) -> AsyncGenerator[FastAPI, None]:
     """Construct and initialize a REST API app along with all its dependencies.
     By default, the core dependencies are automatically prepared but you can also
@@ -103,7 +102,7 @@ async def prepare_rest_app(
 async def prepare_event_subscriber(
     *,
     config: Config,
-    core_override: Optional[tuple[UploadServicePort, FileMetadataServicePort]] = None,
+    core_override: tuple[UploadServicePort, FileMetadataServicePort] | None = None,
 ) -> AsyncGenerator[KafkaEventSubscriber, None]:
     """Construct and initialize an event subscriber with all its dependencies.
     By default, the core dependencies are automatically prepared but you can also
