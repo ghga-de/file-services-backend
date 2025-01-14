b268dcb37997e619260d7085e7ba0ffc24ef4a95
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2025-01-14 16:54:25 +0100

    Update services/ekss/src/ekss/core/envelope_encryption.py
    Co-authored-by: Byron Himes <thebyronhimes@gmail.com>

--- services/ekss/src/ekss/core/envelope_encryption.py
+++ services/ekss/src/ekss/core/envelope_encryption.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Implements functionality for envelope encrytion"""
+"""Implements functionality for envelope encryption"""
 
 from pathlib import Path
 
