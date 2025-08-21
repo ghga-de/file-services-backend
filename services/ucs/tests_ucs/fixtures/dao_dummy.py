285b784d57b7a17157402eb84c89127032ca73c2
TheByronHimes <TheByronHimes@gmail.com>
2025-08-21 09:32:27 +0000

    Revise dummy dao setup for better type detection and completeness

--- services/ucs/tests_ucs/fixtures/dao_dummy.py
+++ services/ucs/tests_ucs/fixtures/dao_dummy.py
@@ -16,114 +16,118 @@
 """A dummy DAO"""
 
 from collections.abc import AsyncIterator, Mapping
+from copy import deepcopy
 from typing import Any, TypeVar
 from unittest.mock import AsyncMock, Mock
 
 from hexkit.custom_types import ID
-from hexkit.protocols.dao import ResourceAlreadyExistsError, ResourceNotFoundError
+from hexkit.protocols.dao import (
+    MultipleHitsFoundError,
+    NoHitsFoundError,
+    ResourceAlreadyExistsError,
+    ResourceNotFoundError,
+)
 from pydantic import BaseModel
 
 from ucs.core.models import FileUpload, FileUploadBox, S3UploadDetails
 
-
-class MockDaoBase:
-    """Assigns sync mock to unused methods"""
-
-    with_transaction = Mock()
-
-
-class MockDaoPubBase(MockDaoBase):
-    """Assigns async mock to unused methods"""
-
-    publish_pending = republish = AsyncMock()
-
-
 DTO = TypeVar("DTO", bound=BaseModel)
 
 
-def get_dao[DTO: BaseModel](*, dto_model: type[DTO], id_field: str):  # noqa: C901
-    """Produce a dummy DAO for the given DTO model and id field"""
+class BaseDummyDao[DTO: BaseModel]:
+    """Base class for dummy DAOs with proper typing"""
 
-    class DummyDao[DTO]:  # type: ignore
-        """Dummy dao that stores data in memory"""
+    _id_field: str
+    publish_pending = AsyncMock()
+    republish = AsyncMock()
+    with_transaction = Mock()
 
-        resources: list
-        with_transaction = Mock()
+    def __init__(self) -> None:
+        self.resources: list[DTO] = []
 
-        def __init__(self):
-            """Initialize the dummy dao publisher"""
-            self.resources = []
+    @property
+    def latest(self) -> DTO:
+        """Return the most recently inserted resource"""
+        return deepcopy(self.resources[-1])
 
-        @property
-        def latest(self) -> DTO:
-            """Return the most recently inserted resource"""
-            return self.resources[-1]
+    async def get_by_id(self, id_: ID) -> DTO:
+        """Get the resource via ID."""
+        for resource in self.resources:
+            if id_ == getattr(resource, self._id_field):
+                return deepcopy(resource)
+        raise ResourceNotFoundError(id_=id_)
 
-        async def get_by_id(self, id_: ID) -> DTO:
-            """Get the box via ID."""
-            for resource in self.resources:
-                if id_ == getattr(resource, id_field):
-                    return resource
+    async def find_one(self, *, mapping: Mapping[str, Any]) -> DTO:
+        """Find the resource that matches the specified mapping."""
+        hits = self.find_all(mapping=mapping)
+        try:
+            dto = await hits.__anext__()
+        except StopAsyncIteration as error:
+            raise NoHitsFoundError(mapping=mapping) from error
+
+        try:
+            _ = await hits.__anext__()
+        except StopAsyncIteration:
+            # This is expected:
+            return dto
+
+        raise MultipleHitsFoundError(mapping=mapping)
+
+    async def find_all(self, *, mapping: Mapping[str, Any]) -> AsyncIterator[DTO]:
+        """Find all resources that match the specified mapping."""
+        for resource in self.resources:
+            if all([getattr(resource, k) == v for k, v in mapping.items()]):
+                yield deepcopy(resource)
+
+    async def insert(self, dto: DTO) -> None:
+        """Insert a resource"""
+        dto_id = getattr(dto, self._id_field)
+        for resource in self.resources:
+            if getattr(resource, self._id_field) == dto_id:
+                raise ResourceAlreadyExistsError(id_=dto_id)
+        self.resources.append(deepcopy(dto))
+
+    async def update(self, dto: DTO) -> None:
+        """Update a resource"""
+        for i, resource in enumerate(self.resources):
+            if getattr(resource, self._id_field) == getattr(dto, self._id_field):
+                self.resources[i] = deepcopy(dto)
+                break
+        else:
+            raise ResourceNotFoundError(id_=getattr(dto, self._id_field))
+
+    async def delete(self, id_: ID) -> None:
+        """Delete a resource by ID"""
+        for i, resource in enumerate(self.resources):
+            if getattr(resource, self._id_field) == id_:
+                del self.resources[i]
+                break
+        else:
             raise ResourceNotFoundError(id_=id_)
 
-        async def find_one(self, *, mapping: Mapping[str, Any]):
-            """Just here to satisfy protocol"""
-            raise NotImplementedError()
+    async def upsert(self, dto: DTO) -> None:
+        """Upsert a resource"""
+        for i, resource in enumerate(self.resources):
+            if getattr(resource, self._id_field) == getattr(dto, self._id_field):
+                self.resources[i] = deepcopy(dto)
+                break
+        else:
+            self.resources.append(deepcopy(dto))
 
-        async def find_all(self, *, mapping: Mapping[str, Any]) -> AsyncIterator[DTO]:
-            """Just here to satisfy protocol"""
-            for resource in self.resources:
-                if all([getattr(resource, k) == v for k, v in mapping.items()]):
-                    yield resource  # type: ignore
 
-        async def insert(self, dto: DTO) -> None:
-            """Insert a resource"""
-            dto_id = getattr(dto, id_field)
-            for resource in self.resources:
-                if getattr(resource, id_field) == dto_id:
-                    raise ResourceAlreadyExistsError(id_=dto_id)
-            self.resources.append(dto)
+def get_dao[DTO: BaseModel](
+    *, dto_model: type[DTO], id_field: str
+) -> type[BaseDummyDao[DTO]]:
+    """Produce a dummy DAO for the given DTO model and id field"""
 
-        async def update(self, dto: DTO) -> None:
-            """Update a resource"""
-            for i, resource in enumerate(self.resources):
-                if getattr(resource, id_field) == getattr(dto, id_field):
-                    self.resources[i] = dto
-                    break
-            else:
-                raise ResourceNotFoundError(id_=getattr(dto, id_field))
+    class DummyDao(BaseDummyDao[DTO]):
+        """Dummy dao that stores data in memory"""
 
-        async def delete(self, id_: ID) -> None:
-            """Delete a resource by ID"""
-            for i, resource in enumerate(self.resources):
-                if getattr(resource, id_field) == id_:
-                    del self.resources[i]
-                    break
-            else:
-                raise ResourceNotFoundError(id_=id_)
-
-        async def upsert(self, dto: DTO) -> None:
-            """Upsert a resource"""
-            for i, resource in enumerate(self.resources):
-                if getattr(resource, id_field) == getattr(dto, id_field):
-                    self.resources[i] = dto
-                    break
-            else:
-                self.resources.append(dto)
+        _id_field: str = id_field
 
     return DummyDao
 
 
-def get_outbox_dao[DTO: BaseModel](*, dto_model: type[DTO], id_field: str):
-    """Produce a dummy outbox DAO for the given DTO model and id field"""
-    DummyDao = get_dao(dto_model=dto_model, id_field=id_field)  # noqa: N806
-
-    class DummyOutboxDao(DummyDao):  # type: ignore
-        publish_pending = republish = AsyncMock()
-
-    return DummyOutboxDao
-
-
-DummyFileUploadBoxDao = get_outbox_dao(dto_model=FileUploadBox, id_field="id")
-DummyFileUploadDao = get_outbox_dao(dto_model=FileUpload, id_field="id")
+DummyFileUploadBoxDao = get_dao(dto_model=FileUploadBox, id_field="id")
+DummyFileUploadDao = get_dao(dto_model=FileUpload, id_field="id")
 DummyS3UploadDetailsDao = get_dao(dto_model=S3UploadDetails, id_field="file_id")
